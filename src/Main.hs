5a44665558f15f2b9b3dbe991690b667f3892dcb
mavenraven.org <developers@mavenraven.org>
2014-06-08 18:23:10 -0400

    no haskell for me, please

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS 
+import Control.Exception
+import Data.IORef
+import Control.Applicative   ((<$>))
+import Data.ByteString.Lazy
+import Data.ByteString
+import Data.Aeson
+import Data.Maybe
+
+manager :: IO (IORef Manager)
+manager = newManager settings >>= newIORef
+  where settings = tlsManagerSettings {
+    managerConnCount       = 100
+  , managerResponseTimeout = Just 2000000 -- 2 secs
+}
+
+ourRequest' = ourRequest { requestHeaders = [("User-Agent", "katurday")] }
+
+ourRequest :: Request
+ourRequest = case (parseUrl "https://api.github.com/search/code?q=addClass+in:file+language:js+repo:jquery/jquery") of
+  Just x -> x
+  Nothing -> error "bad"
+
 main = print "hi"
+
+--getReq :: IO (Either e0 Data.ByteString.ByteString)
+getReq = manager >>= readIORef >>= \man -> toStrict . responseBody <$> httpLbs ourRequest' man
+
+jsonStuff =  getReq  >>= \response -> return $ (fromJust (decode (fromChunks [response]) :: Maybe Value))
+
